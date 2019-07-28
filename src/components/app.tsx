113b654dc7ae386ce8c149ece4e8fe8374c1a326
Khairul Anshar <halo@khairulanshar.com>
2019-07-29 01:49:51 +0800

    build

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -8,7 +8,7 @@ export const App: React.FC<AppProps> = (props) => {
     return (
         <TodoListProvider>
             <div className="container">
-                <h1 className="title">Simple Task List with {props.compiler}, {props.framework} and {props.store}! <a className="btn btn-primary" target="_blank" href="https://github.com/khairulanshar98/reacthooks">source</a></h1>
+                <h1 className="title">Simple Task List with {props.compiler}, {props.framework} and {props.store}! <a className="btn btn-primary" target="_blank" href="https://github.com/khairulanshar98/reactusecontext">source</a></h1>
                 <div className="col-sm-4">
                     <TodoAdd />
                 </div>
